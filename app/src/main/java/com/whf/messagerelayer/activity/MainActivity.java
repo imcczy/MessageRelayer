e800634a19d3e12475663603942bfad1f754b1e3
imcczy <imcczyh@gmail.com>
2017-11-27 20:19:38 +0800

    增加电话挂断和短信通知

--- app/src/main/java/com/whf/messagerelayer/activity/MainActivity.java
+++ app/src/main/java/com/whf/messagerelayer/activity/MainActivity.java
@@ -1,6 +1,10 @@
 package com.whf.messagerelayer.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,13 +14,15 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.whf.messagerelayer.R;
+import com.whf.messagerelayer.service.CallService;
 import com.whf.messagerelayer.utils.NativeDataManager;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private RelativeLayout mSmsLayout, mEmailLayout, mRuleLayout;
     private NativeDataManager mNativeDataManager;
-
+    private Intent CallIntent;
+    final public static int REQUEST_CODE_ASK_CALL_PHONE = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +30,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mNativeDataManager = new NativeDataManager(this);
         initView();
+        int phone = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE);
+        int sms = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.SEND_SMS);
+        if(phone == PackageManager.PERMISSION_DENIED || sms == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CALL_PHONE,Manifest.permission.SEND_SMS},REQUEST_CODE_ASK_CALL_PHONE);
+        }
     }
 
     @Override
@@ -40,14 +51,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 Boolean receiver = mNativeDataManager.getReceiver();
-                if(receiver){
+                if (receiver) {
+                    if (CallIntent != null) {
+                        stopService(CallIntent);
+                    }
                     mNativeDataManager.setReceiver(false);
                     menuItem.setIcon(R.mipmap.ic_send_off);
-                    Toast.makeText(MainActivity.this,"总闸已关闭",Toast.LENGTH_SHORT).show();
-                }else{
+                    Toast.makeText(MainActivity.this, "总闸已关闭", Toast.LENGTH_SHORT).show();
+                } else {
+                    CallIntent = new Intent(MainActivity.this, CallService.class);
+                    startService(CallIntent);
+                    Toast.makeText(getApplicationContext(), "开启电话拦截", Toast.LENGTH_SHORT).show();
                     mNativeDataManager.setReceiver(true);
                     menuItem.setIcon(R.mipmap.ic_send_on);
-                    Toast.makeText(MainActivity.this,"总闸已开启",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "总闸已开启", Toast.LENGTH_SHORT).show();
                 }
                 return true;
             }
@@ -57,7 +74,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem item) {
-                        startActivity(new Intent(MainActivity.this,AboutActivity.class));
+                        startActivity(new Intent(MainActivity.this, AboutActivity.class));
                         return false;
                     }
                 }).setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
---
+++ app/src/main/java/com/whf/messagerelayer/service/CallService.java
@@ -0,0 +1,93 @@
+package com.whf.messagerelayer.service;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import com.android.internal.telephony.ITelephony;
+import com.whf.messagerelayer.confing.Constant;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by imcczy on 2017/11/27.
+ */
+
+public class CallService extends Service{
+    private TelephonyManager telephonyManager;
+    private MyPhoneStateListener myPhoneStateListener;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Log.e("imcczy","test");
+        // 监听电话状态
+        telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        myPhoneStateListener = new MyPhoneStateListener();
+        // 参数1:监听
+        // 参数2:监听的事件
+        telephonyManager.listen(myPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+    }
+
+    private class MyPhoneStateListener extends PhoneStateListener {
+        @Override
+        public void onCallStateChanged(int state, final String incomingNumber) {
+            super.onCallStateChanged(state, incomingNumber);
+            if (state == TelephonyManager.CALL_STATE_RINGING) {
+                Log.e("imcczy",incomingNumber);
+                sendSMS(incomingNumber);
+                endCall();
+
+            }
+        }
+    }
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        telephonyManager.listen(myPhoneStateListener, PhoneStateListener.LISTEN_NONE);
+    }
+
+    public void endCall() {
+        Log.e("imcczy","Begin to end call");
+        //通过反射进行实现
+        try {
+            //1.通过类加载器加载相应类的class文件
+            //Class<?> forName = Class.forName("android.os.ServiceManager");
+            Class<?> loadClass = CallService.class.getClassLoader().loadClass("android.os.ServiceManager");
+            //2.获取类中相应的方法
+            //name : 方法名
+            //parameterTypes : 参数类型
+            Method method = loadClass.getDeclaredMethod("getService", String.class);
+            //3.执行方法,获取返回值
+            //receiver : 类的实例
+            //args : 具体的参数
+            IBinder invoke = (IBinder) method.invoke(null, Context.TELEPHONY_SERVICE);
+            //aidl
+            ITelephony iTelephony = ITelephony.Stub.asInterface(invoke);
+            //挂断电话
+            iTelephony.endCall();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void sendSMS(String phponenum){
+        Context context = getApplicationContext();
+        Intent serviceIntent = new Intent(context, SmsService.class);
+        serviceIntent.putExtra(Constant.EXTRA_MESSAGE_CONTENT,phponenum);
+        context.startService(serviceIntent);
+    }
+}
